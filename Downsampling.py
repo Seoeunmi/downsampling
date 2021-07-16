27085cb63dd8338ad94ae501a7c2312be7c8544f
eunmi <deluv98@naver.com>
2021-07-16 17:50:50 +0900

    upsampling 제거, 경로 저장법 수

--- Downsampling.py
+++ Downsampling.py
@@ -7,7 +7,13 @@ def createFolder(directory):
         if not os.path.exists(directory):
             os.makedirs(directory)
     except OSError:
-        print ('Issue: Creating directory. ' +  directory)
+        print ('Issue: Creating directory. ' + directory)
+
+def load_path():
+    path = os.path.join(os.path.dirname(__file__))
+    if path == "":
+        path = "."
+    return path
 
 def read_path_list(dirname, extention=""):
     try:
@@ -28,13 +34,18 @@ def read_path_list(dirname, extention=""):
 
 def down_sample(input_wav, origin_sr, resample_sr, save_path):
     y, sr = librosa.load(input_wav, sr=origin_sr)
+    if sr != origin_sr:
+        raise Exception("ERROR: Sampling rate isn't %d".format(origin_sr))
+
     resample = librosa.resample(y, sr, resample_sr)
     print("\r original wav sr: {}, original wav shape: {}, resample wav sr: {}, resmaple shape: {}".format(origin_sr, y.shape, resample_sr, resample.shape))
-    createFolder(save_path)
-    sf.write(save_path + '/{}'.format(input_wav[-12:]), resample, resample_sr, format='WAV', endian='LITTLE')
+    sf.write(save_path + '/' + os.path.basename(file_name[i]), resample, resample_sr, format='WAV', endian='LITTLE')
 
-save_path = './data/downsample'
-dir_name = '/home/eunmi/PycharmProjects/downsampling/data/test'
+
+dir_name = '/home/eunmi/PycharmProjects/downsampling/data/'
 file_name = read_path_list(dir_name, extention='wav')
+
 for i in range(len(file_name)):
-    down_sample(file_name[i], 16000, 4000, save_path)
+    save_path = load_path() + '/generate_data' + os.path.dirname(file_name[i].replace(dir_name, "").replace(os.path.basename(file_name[i]), ""))
+    createFolder(save_path)
+    down_sample(file_name[i], 16000, 8000, save_path)
